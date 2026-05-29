046d6c07bfc43077eebd7432d28cc5dd19282cfd
mahimhasan34277-maker <mahimhasan34277@gmail.com>
2026-05-29 23:42:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telebot.async_telebot import AsyncTeleBot
 
-# Config
+# Config - নতুন টোকেন বসানো হয়েছে
 API_ID = 31800390
 API_HASH = "41c07303eb651fccda2ff90957d1ded6"
-BOT_TOKEN = "8839286449:AAEBGJHmPDY4_FAddCA9uVgtz5fXOqBoLXg"
+BOT_TOKEN = "8839286449:AAEl-g-3B9SQ2Vo_FD1EEGw1XwSzXdB2Rms" 
 SOURCE_CHANNEL = -1003280015883
 OWNER_ID = 7034610665
 
@@ -20,6 +20,8 @@ async def start_scraper():
     if not SESSION_STRING:
         print("SESSION_STRING খালি!")
         return
+    
+    # নতুন সেশন স্ট্রিং জেনারেট করে এখানে সেট করতে ভুলবেন না
     client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
     await client.connect()
     
